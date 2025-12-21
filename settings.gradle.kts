56cf2a10e1d3231c6bb7115563f8a92f3fd76dad
anhtayvc205 <anhtayvc205@gmail.com>
2025-12-22 01:34:28 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
+rootProject.name = "oraxen"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+        maven("https://repo.mineinabyss.com/releases")
+    }
+}
+
 dependencyResolutionManagement {
 
     // ✅ TƯƠNG THÍCH Paperweight
@@ -37,3 +47,16 @@ dependencyResolutionManagement {
         create("oraxenLibs").from(files("gradle/oraxenLibs.versions.toml"))
     }
 }
+
+include(
+    "core",
+    "v1_20_R1",
+    "v1_20_R2",
+    "v1_20_R3",
+    "v1_20_R4",
+    "v1_21_R1",
+    "v1_21_R2",
+    "v1_21_R3",
+    "v1_21_R4",
+    "v1_21_R5"
+)
